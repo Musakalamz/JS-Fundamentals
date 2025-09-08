21b4567fc7aef8e1d239516b1d93a531ce36a96c
Musakalamz <ogunsolumusa@gmail.com>
2025-09-08 17:54:12 +0100

    Arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,6 +2,10 @@ if (process.argv.length < 3) {
   console.log("No argument");
 }
 
-process.argv.length === 3
-  ? console.log("Argument found")
-  : console.log("Arguments found");
+if (process.argv.length === 3) {
+  console.log("Argument found");
+}
+
+if (process.argv.length === 4) {
+  console.log("Arguments found");
+}
