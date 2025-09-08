d572b64dd394356209506f58db85d50edf501dfe
Musakalamz <ogunsolumusa@gmail.com>
2025-09-08 21:36:46 +0100

    An Integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,3 +1,3 @@
 Number(process.argv[2])
-  ? console.log(`My number: ${Math.floor(process.argv[2])}  `)
+  ? console.log(`My number: ${Math.floor(process.argv[2])}`)
   : console.log("Not a number");
