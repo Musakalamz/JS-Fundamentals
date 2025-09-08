2d55c2183a3df65b192dea0cced42cb49e0b08e4
Musakalamz <ogunsolumusa@gmail.com>
2025-09-08 17:16:36 +0100

    Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
+const sentences = [firstLine, secondLine, thirdLine];
+
+for (let index = 0; index < sentences.length; index++) {
+  console.log(sentences[index]);
+}
