230c5bbf00ede29a26e501f0e8f2c8e494c86d5c
Musakalamz <ogunsolumusa@gmail.com>
2025-09-09 09:29:26 +0100

    Add

---
+++ 9-add.js
@@ -0,0 +1,10 @@
+const firstNum = parseInt(process.argv[2]);
+const secondNum = parseInt(process.argv[3]);
+
+function add(a, b) {
+  const sum = a + b;
+  console.log(sum);
+  //   return ;
+}
+
+add(firstNum, secondNum);
